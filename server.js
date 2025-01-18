1cc5e47515d221935bea9a38d810624599964cad
Roushan Kumar <your_email@example.com>
2025-01-19 00:43:31 +0530

    kaam start kro

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express= require ('express');
+const app=express();
+app.listen(3000, () => {
+   console.log("server started at port no : 3000")
+});
+
+app.get('/',(req,res) => {
+ console.log("hello ji ,kaise ho sb ");
+})
