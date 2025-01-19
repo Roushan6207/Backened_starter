492f4b937e76293ac46417f785a1eff1d03a7ba5
Roushan Kumar <your_email@example.com>
2025-01-19 15:35:40 +0530

    learned : get and post request

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 
+//server   created...
 const express= require ('express');
 const app=express();
+
+//used to parse req.body in express.-> put or post
+const bodyParser= require("body-parser");
+
+// specifivically parse json data & add it to the request.body object
+app.use(bodyParser.json());
+
+//activate the server on 3000 port..
 app.listen(3000, () => {
    console.log("server started at port no : 3000")
 });
 
+// routes defined...
+
+//get request..
 app.get('/',(req,res) => {
- console.log("hello ji ,kaise ho sb ");
+res.send("hello sir kaise ho");
+}) 
+
+//post request..
+app.post('/api/van', (request,response) =>{
+   const {name,brand}= request.body;
+   console.log(name);
+   console.log(brand);
+   response.send("car mil gye");
 })
