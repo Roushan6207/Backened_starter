4e54ad8ffa87b4f859743aa3848e57314b4deae4
Roushan Kumar <your_email@example.com>
2025-01-21 16:20:11 +0530

    Add new folder and files

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+require("dotenv").config();
+
+
+const dbconnect = () => {
+  mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  
+  .then(() => {
+    console.log("Database connection ho gya h");
+  })
+  .catch((error) => {
+    console.log("Database connection failed");
+    console.log(error.message);
+    process.exit(1);
+  });
+}
+
+module.exports = dbconnect;
