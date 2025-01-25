11da6c231c15afdeccdf779e48252e149e104fe4
Roushan Kumar <your_email@example.com>
2025-01-26 02:29:51 +0530

    learn about get request

--- routes/todos.js
+++ routes/todos.js
@@ -3,10 +3,12 @@ const router=express.Router();
 
 //import controllers...
 const {createTodo}=require("../controllers/createTodo");
-
+const {getTodo,getTodoById}=require("../controllers/getTodo");
 
 //define api routes
 
 router.post("/createTodo",createTodo);
+router.get("/getTodo",getTodo);
+router.get("/getTodo/:id",getTodoById);
 
 module.exports=router;
